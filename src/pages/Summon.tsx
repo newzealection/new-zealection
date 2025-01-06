7e37e8a1d67b86044929171122f8cc4c4bb233e8
newzealection <newzealection@gmail.com>
2025-01-06 16:54:06 +1300

    Update Summon.tsx

--- src/pages/Summon.tsx
+++ src/pages/Summon.tsx
@@ -1,18 +1,21 @@
+import Image from 'next/image';
 import { ComingSoon } from "@/components/ComingSoon";
 import { Navbar } from "@/components/Navbar";
 
 const Summon = () => {
   return (
-    <div 
-      className="min-h-screen relative"
-      style={{
-        backgroundImage: 'url(https://i.imghippo.com/files/hRhQ4091yk.png)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
+    <div className="min-h-screen relative">
+      <Image 
+        src="https://i.imghippo.com/files/hRhQ4091yk.png" 
+        alt="Background" 
+        layout="fill" 
+        objectFit="cover" 
+        quality={100} 
+        className="absolute z-0"
+      />
+      
       {/* Blur overlay */}
-      <div className="absolute inset-0 backdrop-blur-[2px] bg-black/30" />
+      <div className="absolute inset-0 backdrop-blur-[2px] bg-black/30 z-[1]" />
       
       {/* Content */}
       <div className="relative z-10">
