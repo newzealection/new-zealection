1c6e466775d0ccdf8da9d427df29a434bc46601f
newzealection <newzealection@gmail.com>
2025-01-06 18:57:40 +1300

    Update Marketplace.tsx

--- src/pages/Marketplace.tsx
+++ src/pages/Marketplace.tsx
@@ -1,13 +1,24 @@
 import { ComingSoon } from "@/components/ComingSoon";
 import { Navbar } from "@/components/Navbar";
 
-const Marketplace = () => {
+const Summon = () => {
   return (
-    <>
-      <Navbar />
-      <ComingSoon feature="Marketplace" />
-    </>
+    <div className="min-h-screen relative">
+      <div 
+        className="absolute inset-0"
+        style={{
+          backgroundImage: 'url(https://i.imghippo.com/files/nnGQ9211Fo.webp)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+      
+      {/* Content */}
+      <div className="relative z-10">
+        <Navbar />
+        <ComingSoon feature="Marketplace" />
+      </div>
+    </div>
   );
 };
-
 export default Marketplace;
